4702a3db8b244db388ca649692a89fee4b4dae0a
Sofrin <85531415+sofrin@users.noreply.github.com>
2023-11-19 23:14:21 +0700

    feat: add mongodb connection

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -1,17 +1,19 @@
-import { Guild } from "discord.js";
-import GuildModel from "../schemas/Guild";
-import { BotEvent } from "../types";
+import { Guild } from 'discord.js';
+import GuildModel from '../schemas/Guild';
+import { BotEvent } from '../types';
 
 const event: BotEvent = {
-    name: "guildCreate",
-    execute: (guild : Guild) => {
-        let newGuild = new GuildModel({
-            guildID: guild.id,
-            options: {},
-            joinedAt: Date.now()
-        })
-        newGuild.save()
-    }
-}
+	name: 'guildCreate',
+	execute: (guild: Guild) => {
+		let newGuild = new GuildModel({
+			guildID: guild.id,
+			options: {},
+			joinedAt: Date.now(),
+			bids: [],
+			token: '',
+		});
+		newGuild.save();
+	},
+};
 
 export default event;
--- src/slashCommands/hobby.ts
+++ src/slashCommands/hobby.ts
@@ -1,35 +1,42 @@
-import { SlashCommandBuilder, ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle } from "discord.js"
-import { SlashCommand } from "../types";
+import {
+	SlashCommandBuilder,
+	ModalBuilder,
+	ActionRowBuilder,
+	TextInputBuilder,
+	TextInputStyle,
+} from 'discord.js';
+import { SlashCommand } from '../types';
+import { setGuildToken } from '../functions';
 
-const command : SlashCommand = {
-    command: new SlashCommandBuilder()
-        .setName("hobby")
-        .setDescription("What is your hobby?")
-    ,
-    execute: async (interaction) => {
-        const modal = new ModalBuilder()
-            .setCustomId("hobby")
-            .setTitle("What is your hobby?")
+const command: SlashCommand = {
+	command: new SlashCommandBuilder()
+		.setName('token')
+		.setDescription('What is your token?'),
+	execute: async (interaction) => {
+		const modal = new ModalBuilder()
+			.setCustomId('token')
+			.setTitle('What is your token?');
 
-		const hobbiesInput = new TextInputBuilder()
-			.setCustomId('hobbiesInput')
-			.setLabel("What's some of your favorite hobbies?")
+		const tokenInput = new TextInputBuilder()
+			.setCustomId('tokenInput')
+			.setLabel("What's some of your favorite token?")
 			.setStyle(TextInputStyle.Paragraph);
 
-		const secondActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(hobbiesInput);
+		const secondActionRow =
+			new ActionRowBuilder<TextInputBuilder>().addComponents(tokenInput);
 
 		modal.addComponents(secondActionRow);
 
 		await interaction.showModal(modal);
-    },
-    modal: async (interaction) => {
-        await interaction.deferReply({ ephemeral: true });
-        
-        const hobbies = interaction.fields.getTextInputValue('hobbiesInput');
+	},
+	modal: async (interaction) => {
+		await interaction.deferReply({ ephemeral: true });
 
-        await interaction.editReply({ content: `So, your hobby is ${hobbies}!` });
-    },
-    cooldown: 5
-}
+		const token = interaction.fields.getTextInputValue('tokenInput');
+		setGuildToken(interaction.guild, token);
+		await interaction.editReply({ content: `So, your token is ${token}!` });
+	},
+	cooldown: 5,
+};
 
-export default command
+export default command;
